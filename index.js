f4ab37891fe7ebde98893f80c51aa5b89df7b2ad
Christof Marti <chrmarti@microsoft.com>
2016-07-26 07:45:06 -0700

    Getter

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const t = require('babel-types');
 const template = require('babel-template');
 
 const code = `
-var c = { a: 'b' };
-var c2 = c.d = 1;
+var map1 = { a: 1, b: 2, c: 3 };
+var map2 = map2.b = 50;
+map1.b;
+map2.b;
 `;
 
 const objectExpression = template(`Immutable.Map(OBJECT_EXPRESSION)`);
 const assignmentExpression = template(`OBJECT.set(PROPERTY, EXPRESSION)`);
+const memberExpression = template(`OBJECT.get(PROPERTY)`);
 
 function plugin({ types: t }) {
     const seen = new Set();
@@ -29,6 +32,14 @@ function plugin({ types: t }) {
                         OBJECT: left.object,
                         PROPERTY: t.stringLiteral(left.property.name),
                         EXPRESSION: path.node.right
+                    }));
+                }
+            },
+            MemberExpression(path) {
+                if (!t.isCallExpression(path.parentPath.node)) {
+                    path.replaceWith(memberExpression({
+                        OBJECT: path.node.object,
+                        PROPERTY: t.stringLiteral(path.node.property.name)
                     }));
                 }
             }
